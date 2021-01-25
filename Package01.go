ae25a2727c1545b418574b2de9208b1fe9744f26
Arun Aravind <33226350+kochukulam@users.noreply.github.com>
2021-01-25 13:13:38 +0530

    Add files via upload

---
+++ Package01.go
@@ -0,0 +1,13 @@
+//Every program is made up of Package.
+//Program starts with Package main.
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("My fav number is: ", rand.Intn(10))
+}
